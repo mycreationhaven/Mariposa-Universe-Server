b1f54a5fe3f4f521a4aac0be24ec4421a6ddd6dc
Arkovia <create@mycreationhaven.com>
2026-09-20 21:21:46 -0500

    Add authenticated sessions and reconnect support: eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,3 @@
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
-export default tseslint.config({ignores:['dist','node_modules','drizzle']},js.configs.recommended,...tseslint.configs.recommended,{languageOptions:{globals:{EventTarget:'readonly',Event:'readonly',CustomEvent:'readonly',performance:'readonly',crypto:'readonly',console:'readonly',runOnStartup:'readonly'}},rules:{'@typescript-eslint/no-unused-vars':['error',{argsIgnorePattern:'^_'}]}});
+export default tseslint.config({ignores:['dist','node_modules','drizzle']},js.configs.recommended,...tseslint.configs.recommended,{languageOptions:{globals:{EventTarget:'readonly',Event:'readonly',CustomEvent:'readonly',performance:'readonly',crypto:'readonly',console:'readonly',fetch:'readonly',runOnStartup:'readonly'}},rules:{'@typescript-eslint/no-unused-vars':['error',{argsIgnorePattern:'^_'}]}});
